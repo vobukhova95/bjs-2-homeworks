8db3d0b8e5919c76bec47e27a36c127b73d17c1b
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-08-16 00:16:24 +0700

    The homework is done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,42 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	const d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return [];
+	}
+
+	if (d === 0) {
+		return [-b / (2 * a)];
+	}
+
+	const sqrtD = Math.sqrt(d);
+	const root1 = (-b + sqrtD) / (2 * a);
+	const root2 = (-b - sqrtD) / (2 * a);
+	return [root1, root2];
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	percent = Number(percent);
+	contribution = Number(contribution);
+	amount = Number(amount);
+	countMonths = Number(countMonths);
+
+	if (
+		isNaN(percent) ||
+		isNaN(contribution) ||
+		isNaN(amount) ||
+		isNaN(countMonths)
+	) {
+		return false;
+	}
+
+	const monthlyPercentage = (percent / 100) / 12;
+	const creditAmount = amount - contribution;
+
+	const monthlyPayment = creditAmount *
+		(monthlyPercentage + (monthlyPercentage / (((1 + monthlyPercentage) ** countMonths) - 1)));
+
+	return Number((monthlyPayment * countMonths).toFixed(2));
 }
