c4aa611184c71037705b5c8e74213c30319722d2
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-08-28 00:29:17 +0700

    The HW is done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
+"use strict";
+
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+	return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	const filtered = users.filter(user => user.gender === gender);
+	if (filtered.length === 0) {
+		return 0;
+	}
+	const totalAge = filtered.reduce((acc, user) => acc + user.age, 0);
+	return totalAge / filtered.length;
 }
