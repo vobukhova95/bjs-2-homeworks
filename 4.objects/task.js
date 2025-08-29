e7a008e2e9ba7fccc9043163effd36d9c6844df4
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-08-29 23:35:48 +0700

    The HW is done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
+"use strict";
+
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty("marks")) {
+		this.marks.push(...marks);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!(this.hasOwnProperty("marks")) || this.marks.length === 0) {
+		return 0;
+	}
+	return this.marks.reduce((acc, mark) => acc + mark / this.marks.length, 0);
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
