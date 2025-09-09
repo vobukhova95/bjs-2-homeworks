c98023ba6f4242b0646a47d3716840407c49a501
Veronika Obukhova <vera.obukhova.95@gmail.com>
2025-09-10 00:25:09 +0700

    The HW is done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,59 @@
+"use strict";
+
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+	let cache = [];
+
+	function wrapper(...args) {
+		const hash = md5(args.join(","))
+		let objectInCache = cache.find((item) => item.hash === hash);
+		if (objectInCache) {
+			console.log("Из кеша: " + objectInCache.result);
+			return "Из кеша: " + objectInCache.result;
+		}
+
+		let result = func(...args);
+		cache.push({
+			hash,
+			result
+		});
+		if (cache.length > 5) {
+			cache.shift();
+		}
+		console.log("Вычисляем: " + result);
+		return "Вычисляем: " + result;
+	}
+	return wrapper;
 }
+
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId = null;
+	let isFirstCall = true;
+
+	function wrapper(...args) {
+		wrapper.allCount++;
+
+		if (isFirstCall) {
+			func(...args);
+			wrapper.count++;
+			isFirstCall = false;
+			return;
+		}
+
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+
+		timeoutId = setTimeout(() => {
+			func(...args);
+			wrapper.count++;
+		}, delay);
+	}
+
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	return wrapper;
 }
